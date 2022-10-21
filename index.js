cc596a3c3dea93b2027fcc2f043b31d87f32fa04
Robert <bonglrob@uw.edu>
2022-10-20 23:14:38 -0700

    edits

--- index.js
+++ index.js
@@ -7,13 +7,13 @@
  * You can navigate between dishes by clicking on the button.
  * You can click on the moon icon to toggle between day and night mode and
  * keypress any character to spam new elements on the page.
- **/
+ * */
 "use strict";
+/** keeps all the functions within local scope */
 (function() {
-  // keeps all the functions within local scope
   window.addEventListener("load", init);
 
-  /* initial document event handlers setup */
+  /** initial document event handlers setup */
   function init() {
     let buttons = qsa('button');
     let nightModeIcon = qs('#night-mode-icon');
@@ -24,22 +24,22 @@
     nightModeIcon.addEventListener('click', toNightMode);
   }
 
-  /* retrieves element id */
+  /** retrieves element id */
   function id(id) {
     return document.getElementById(id);
   }
 
-  /* retrieves element */
+  /** retrieves element */
   function qs(selector) {
     return document.querySelector(selector);
   }
 
-  /* retrieves element array */
+  /** retrieves element array */
   function qsa(selector) {
     return document.querySelectorAll(selector);
   }
 
-  /* switches between recipes */
+  /** switches between recipes */
   function nextRecipe() {
     let section = qsa('section');
     for (let i = 0; i < section.length; i++) {
@@ -47,21 +47,21 @@
     }
   }
 
-  /* italizes the header after keypress */
+  /** italizes the header after keypress */
   function konamiCode() {
     const header = qs('header');
     header.appendChild(createFoodScript());
     header.classList.add('konamiCode');
   }
 
-  /* Adds new string to spam the page */
+  /** Adds new string to spam the page */
   function createFoodScript() {
     let paragraph = document.createElement('p');
     paragraph.textContent = "FOOD";
     return paragraph;
   }
 
-  /* Switches the theme of the page to be easier read both night and day */
+  /** Switches the theme of the page to be easier read both night and day */
   function toNightMode() {
     let body = qs('body');
     let nav = qs('nav');
@@ -73,12 +73,12 @@
     header.classList.toggle('nightModeHead');
     article.classList.toggle('nightModeArt');
     sections.forEach(section => {
-      section.classList.toggle('nightModeSec')
+      section.classList.toggle('nightModeSec');
     });
     swapMoonIcon(body);
   }
 
-  /* switches icon from moon to sun and vice versa */
+  /** switches icon from moon to sun and vice versa */
   function swapMoonIcon(body) {
     let img = id('night-mode-icon');
     if (body.classList.contains('nightModeBody')) {
